026c08e760e267a3b77199fc822ff2427d25a941
Eduardo Martins Ribeiro <98466110+edumartinsrib@users.noreply.github.com>
2022-06-26 00:22:32 -0300

    [INIT] first commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('Olá mundo ok')
+ /*  res.render('index', { title: 'OK' }); */
 });
 
 module.exports = router;
